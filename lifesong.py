7dccf91d8907e75a93f884772c866f613d44268c
Johan Meiring <johan@spiraleye.com>
2013-02-12 21:48:48 +0200

    Adds license and readme

--- lifesong.py
+++ lifesong.py
@@ -1,4 +1,27 @@
-# Based on the accepted StackOverflow answer at http://stackoverflow.com/questions/6011115/doc-to-pdf-using-python
+# lifesong.py - Python script for converting Microsoft Word documents
+# to PDF format in bulk.  Currently only known to work on Windows
+# with MS Office installed.  Also requires "comtypes" Python module.
+#
+# Basic usage: python lifesong.py -i "<indir>" -o "<outdir>" [--replace]
+#
+# Copyright (C) 2013  Johan Meiring
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Based on the accepted StackOverflow answer at
+# http://stackoverflow.com/questions/6011115/doc-to-pdf-using-python
+
 import sys
 import os
 import comtypes.client
